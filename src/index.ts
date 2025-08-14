4ec84650da699acf68bf1bf2e7b7243a8569419a
Quinn Keaveney <quinn@liiift.studio>
2025-08-13 23:21:51 -0700

    Add main index file with exports

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { AdvancedRefArray } from './AdvancedRefArray'
+export { useSanityClient } from './hooks/useSanityClient'
+
+// Re-export default
+export { default } from './AdvancedRefArray'
